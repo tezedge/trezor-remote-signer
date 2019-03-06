42e7df43c2a05e8b5b44adf205ddb125db14149b
Juraj Selep <jurajselep@gmail.com>
2019-03-06 16:24:45 +0100

    Added USB support for trezor-remote-signer service

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 docker-compose up
+
+docker exec 2502d335f68d /bin/sh -c "trezorctl list"
+
--- tezos-client/entrypoint.sh
+++ tezos-client/entrypoint.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 export TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y
 
+# remote Tezos node 
 ADDRESS=zeronet.simplestaking.com
 PORT=3000
 
+# BIP32 path for Trezor T
+HW_WALLET_HD_PATH='"m/44'\''/1729'\''/3'\''"'
+
+# register/get public key hash for BIP32 path
+PUBLIC_KEY_HASH="$(
+    curl --request POST http://trezor-remote-signer:5000/register --silent \
+         --header 'Content-Type: application/json' \
+         --data $HW_WALLET_HD_PATH  | jq -r '.pkh' )"
+
+echo "Address: $PUBLIC_KEY_HASH  path: $HW_WALLET_HD_PATH"
+
 # tezos-client --addr $ADDRESS --port $PORT --tls set man  -v 3
 echo $(tezos-client --addr $ADDRESS --port $PORT --tls get timestamp)
-echo $(tezos-client --addr $ADDRESS --port $PORT --tls get balance for tz1Q1FsKNmNyhgbxhtbet2qzJPskPtT8s5nH)
+echo $(tezos-client --addr $ADDRESS --port $PORT --tls get balance for $PUBLIC_KEY_HASH)
